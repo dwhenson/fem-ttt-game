eb246e8c7782b7edefc1f980c1a4e237b97766a6
dwhenson <dave.henson@gmail.com>
2022-10-11 10:57:40 +0100

    ♻️ REFACTOR: computer moves functions

--- src/utils/computerMoves.js
+++ src/utils/computerMoves.js
@@ -1,85 +1,70 @@
 import LINES from "../constants/winningLines";
 
+/**
+ * Picks a random square from the reminaing squares on the board
+ * @param   {array}  squares  The array of squares already used in the game
+ * @return  {number}          The index of a random remaining square to select
+ */
 function pickRandomSquare(squares) {
   const squaresLeft = squares
     .map((square, index) => (square === null ? index : null))
-    .filter((square) => !!square);
-
-  let choice = squaresLeft[Math.floor(Math.random() * squaresLeft.length)];
-  return choice;
+    .filter((square) => typeof square === "number");
+  return squaresLeft[Math.floor(Math.random() * squaresLeft.length)];
 }
 
+/**
+ * Picks the center square if it is not already taken
+ * @param   {array}  squares  The array of squares already used in the game
+ * @return  {number}          The index of the center square
+ */
 function pickCenterSquare(squares) {
-  let choice = null;
-  if (!squares[4]) {
-    choice = 4;
-  }
-  return choice;
+  return !squares[4] ? 4 : null;
 }
 
-function offensiveComputerMove(squares, marker) {
-  const computerMarker = marker === "X" ? "O" : "X";
-  let choice = null;
-  const squaresUsed = squares.map((square, index) =>
-    square === computerMarker ? index : null
-  );
-
-  for (const line of LINES) {
-    let count = [];
-    for (const number of squaresUsed) {
-      if (line.includes(number)) {
-        count.push(number);
-        if (count.length === 2) {
-          const possibleChoice = line.filter(
-            (number) => !count.includes(number)
-          )[0];
-          if (squares[possibleChoice]) {
-            continue;
-          } else {
-            choice = possibleChoice;
-          }
-        }
-      }
-    }
-  }
-  return choice;
-}
-
-function defensiveComputerMove(squares, marker) {
-  let choice = null;
+function findCriticalSquare(squares, marker) {
   const squaresUsed = squares.map((square, index) =>
     square === marker ? index : null
   );
+  return LINES.find(
+    (line) => squaresUsed.filter((number) => line.includes(number)).length === 2
+  )?.filter(
+    (number) => !squaresUsed.includes(number) && squares[number] === null
+  )[0];
+}
 
-  for (const line of LINES) {
-    let count = [];
-    for (const number of squaresUsed) {
-      if (line.includes(number)) {
-        count.push(number);
-        if (count.length === 2) {
-          const possibleChoice = line.filter(
-            (number) => !count.includes(number)
-          )[0];
-          if (squares[possibleChoice]) {
-            continue;
-          } else {
-            choice = possibleChoice;
-          }
-        }
-      }
-    }
-  }
-  return choice;
+/**
+ * If two opposing markers found on one row, select remaining square
+ * @param   {squares}  squares  The array of squares used in the game
+ * @param   {marker}  marker   The human player marker
+ * @return  {number}           The index of the square to choose
+ */
+function defensiveComputerMove(squares, marker) {
+  return findCriticalSquare(squares, marker);
+}
+
+/**
+ * If the game can be won, choose the winning square
+ * @param   {squares}  squares  The array of squares used in the game
+ * @param   {marker}  marker   The human player marker
+ * @return  {number}           The index of the square to choose
+ */
+function offensiveComputerMove(squares, marker) {
+  console.log("offense");
+  const computerMarker = marker === "X" ? "O" : "X";
+  return findCriticalSquare(squares, computerMarker);
 }
 
 export default function computerMove(squares, marker) {
-  if (offensiveComputerMove(squares, marker) !== null) {
-    return offensiveComputerMove(squares, marker);
-  } else if (defensiveComputerMove(squares, marker) !== null) {
-    return defensiveComputerMove(squares, marker);
-  } else if (pickCenterSquare(squares) !== null) {
-    return pickCenterSquare(squares);
-  } else {
-    return pickRandomSquare(squares);
+  let squareToChoose = offensiveComputerMove(squares, marker);
+  if (!squareToChoose) {
+    squareToChoose = defensiveComputerMove(squares, marker);
   }
+  if (!squareToChoose) {
+    squareToChoose = pickCenterSquare(squares);
+  }
+  if (!squareToChoose) {
+    squareToChoose = pickRandomSquare(squares);
+  }
+  console.log(squareToChoose);
+  return squareToChoose;
 }
